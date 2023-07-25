9015a57aef9ef224e8d18309020ec13de71cc2e3
rioquentin <quentin.rio.pro@gmail.com>
2023-07-25 09:39:38 +0000

    Task 2 - Fixed pycode style and documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+
 """
 Task 2 - Find matric shape
 """
 
 def matrix_shape(matrix):
     """
-    This calculate the shape of a matrix using isinstance function in case there is nested list
+    This calculate the shape of a matrix using 
+    isinstance function in case there is nested list
     """
     shape = []
     while isinstance(matrix, list):
