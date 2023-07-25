4852f58bb0d704b79dd6327a36af1633a91762fa
rioquentin <quentin.rio.pro@gmail.com>
2023-07-25 11:49:08 +0000

    Task 5 - add 2d Matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -9,18 +9,18 @@ def add_matrices2D(mat1, mat2):
     """
     add matrices
     """
-    
+
     matrix_shape = __import__('2-size_me_please').matrix_shape
-    
+
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    
+
     result = []
-    
+
     for i in range(len(mat1)):
-        res =[]
+        res = []
         for x in range(len(mat1[0])):
             res.append(mat1[i][x] + mat2[i][x])
         result.append(res)
-            
+
     return result
