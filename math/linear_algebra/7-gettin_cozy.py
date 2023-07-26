76b526ff1a54070a0bc272cefbc0a1c3d0f60794
rioquentin <quentin.rio.pro@gmail.com>
2023-07-26 17:37:23 +0000

    Task 7 - Concatenate two matrices by axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -10,10 +10,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Concatenate matrices by axis
     """
 
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
+    elif axis != 0 and len(mat1) == len(mat2):
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     else:
-        result = []
-        for i in range(len(mat1)):
-            result.append(mat1[i] + mat2[i])
-        return result
+        return None
