b2ad1dc87bdb31372e920219e4272dc76237e9ce
rioquentin <quentin.rio.pro@gmail.com>
2023-07-27 20:50:44 +0000

    Task 14 - Perform matrix multiply with numpy

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Task 14 - Saddle Up
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Perform matrix multiplication with numpy
+    """
+
+    return np.matmul(mat1, mat2)
