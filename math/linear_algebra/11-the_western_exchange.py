fdee746fbb306b452a0c1a7c580b5d8ebd0bf7b3
rioquentin <quentin.rio.pro@gmail.com>
2023-07-27 14:20:49 +0000

    Task 11 - Transpose a matrix with numpy

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Task 11 - The Western Exchange
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transpose a matrix with numpy
+    """
+
+    return matrix.transpose()
