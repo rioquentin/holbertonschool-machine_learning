43a64e19518d87ef8981d01f57b1836e1bf114d3
rioquentin <quentin.rio.pro@gmail.com>
2023-07-31 13:50:18 +0000

    Task 1 - Plot graph as point with scatter function

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,5 +4,9 @@ import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-plt.plot(y)
+plt.plot(y, color='red')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Line graph of y = x^3')
+plt.grid(True)
 plt.show()
