f7498214f4b98378da03266a9b0143b9d6d13c82
rioquentin <quentin.rio.pro@gmail.com>
2023-07-31 14:23:42 +0000

    Task 4 - Make a Hist graph

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -5,4 +5,8 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-# your code here
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+plt.show()
