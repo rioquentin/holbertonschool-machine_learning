bc754d4420002014965ad82d0b45a4a5ef28ef11
rioquentin <quentin.rio.pro@gmail.com>
2023-08-02 09:30:20 +0000

    Task 9 - Perform sigma operation

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,7 +5,6 @@
 Returns:
     result
 """
-import numpy as np
 
 
 def summation_i_squared(n):
@@ -13,5 +12,5 @@ def summation_i_squared(n):
     Create an array from 1 to n and then sum it up with **2
     """
     if not isinstance(n, int) or n < 1:
-        return None
-    return np.sum((np.arange(1, n + 1)) ** 2)
+        return 0
+    return n**2 + summation_i_squared(n - 1)
