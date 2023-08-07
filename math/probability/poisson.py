90655e14663b81d6ef2049cd4762b61e3403005f
rioquentin <quentin.rio.pro@gmail.com>
2023-08-07 22:21:42 +0000

    Task 2 - Fixed pycodestyle

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -9,7 +9,7 @@ class Poisson:
     """
     Poisson class
     """
-    
+
     e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
@@ -24,38 +24,39 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
 
             self.lambtha = sum(data) / len(data)
-            
+
     def factorial(self, i):
         """
         Function to calculate the factorial of a number
         """
-        
+
         return 1 if i == 0 else i * self.factorial(i - 1)
 
-        
     def pmf(self, k):
         """
         Function to calculate the pmf of the data
         """
-        
+
         if not isinstance(k, int):
             k = int(k)
         if k < 0:
             return 0
         factorial_k = self.factorial(k)
         return (self.e ** (-self.lambtha) * self.lambtha ** k) / factorial_k
-    
+
     def cdf(self, k):
         """
         Function to calculate the cdf of the data
         """
+
         cdf = 0
-        
+
         if not isinstance(k, int):
             k = int(k)
         if k < 0:
             return 0
         for i in range(0, k + 1):
             factorial_i = self.factorial(i)
-            cdf += (self.e ** (-self.lambtha) * (self.lambtha ** i)) / factorial_i
+            cdf += (self.e ** (-self.lambtha) *
+                    (self.lambtha ** i)) / factorial_i
         return cdf
