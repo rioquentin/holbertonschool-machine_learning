f656f54c9123865c2318da5d7fe31a9d770decf2
rioquentin <quentin.rio.pro@gmail.com>
2023-08-07 22:18:30 +0000

    Task 2 - Added a factorial function and a function to calculate the cdf

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -9,6 +9,8 @@ class Poisson:
     """
     Poisson class
     """
+    
+    e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
         if data is None:
@@ -22,17 +24,38 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
 
             self.lambtha = sum(data) / len(data)
+            
+    def factorial(self, i):
+        """
+        Function to calculate the factorial of a number
+        """
+        
+        return 1 if i == 0 else i * self.factorial(i - 1)
 
+        
     def pmf(self, k):
         """
-        FUnction to calculate the pmf of the data
+        Function to calculate the pmf of the data
         """
-        e = 2.7182818285
-        factorial_k = 1
+        
         if not isinstance(k, int):
             k = int(k)
         if k < 0:
             return 0
-        for i in range(1, k + 1):
-            factorial_k *= i
-        return (e ** (-self.lambtha) * self.lambtha ** k) / factorial_k
+        factorial_k = self.factorial(k)
+        return (self.e ** (-self.lambtha) * self.lambtha ** k) / factorial_k
+    
+    def cdf(self, k):
+        """
+        Function to calculate the cdf of the data
+        """
+        cdf = 0
+        
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        for i in range(0, k + 1):
+            factorial_i = self.factorial(i)
+            cdf += (self.e ** (-self.lambtha) * (self.lambtha ** i)) / factorial_i
+        return cdf
