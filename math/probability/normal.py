5b94c4b667faabcc49516edb414c63abc9b1219b
rioquentin <quentin.rio.pro@gmail.com>
2023-08-08 13:38:45 +0000

    Task 6 - Create the normal class

---
+++ math/probability/normal.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Task 6 - Initialize Normal class
+"""
+
+
+class Normal:
+    """
+    Normal class
+    """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if stddev <= 0:
+            raise ValueError("stddev must be a positive value")
+
+        if data is None:
+            if mean == 0:
+                raise ValueError(
+                    "mean cannot be zero when data is not provided")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.mean = float(sum(data) / len(data))
+            self.stddev = (
+                sum((x - self.mean)**2 for x in data) / (len(data) - 1)) ** 0.5
