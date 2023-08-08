3c6c4ff69f339d05053044e8c45593e3c90af06a
rioquentin <quentin.rio.pro@gmail.com>
2023-08-08 13:55:35 +0000

    Task 7 - Create the functions to get the x and z score

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -27,3 +27,17 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             self.stddev = float(
                 (sum((x - self.mean)**2 for x in data) / len(data)) ** 0.5)
+
+    def z_score(self, x):
+        """
+        Function to calculate the z-score
+        """
+
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Function to calculate the x-score
+        """
+
+        return self.mean + z * self.stddev
