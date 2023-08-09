8f25ebeefe92eaa5739a5cb2d721f9bd1fdb045f
rioquentin <quentin.rio.pro@gmail.com>
2023-08-09 13:29:30 +0000

    Task 8 - Create pdf function

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -9,6 +9,9 @@ class Normal:
     """
     Normal class
     """
+    
+    pi = 3.1415926536
+    e = 2.7182818285
 
     def __init__(self, data=None, mean=0., stddev=1.):
         if stddev <= 0:
@@ -41,3 +44,10 @@ class Normal:
         """
 
         return self.mean + z * self.stddev
+    
+    def pdf(self, x):
+        sqrt_2_pi = (2 * self.pi) ** 0.5
+        coefficient = 1 / (self.stddev * sqrt_2_pi)
+        exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
+        pdf_value = coefficient * (self.e ** exponent)
+        return pdf_value 
