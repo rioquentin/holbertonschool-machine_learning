3e5a20862a102ce16b5c468c62fdb2da6a9ab7f0
rioquentin <quentin.rio.pro@gmail.com>
2023-08-10 22:46:07 +0000

    Task 11 - Create pmf function

--- math/probability/main.py
+++ math/probability/main.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
+
 import numpy as np
 Binomial = __import__('binomial').Binomial
 
-try:
-    Binomial(n=0)
-    print('FAIL')
-except ValueError as e:
-    print(e)
-try:
-    Binomial(n=-1)
-    print('FAIL')
-except ValueError as e:
-    print(e)
+np.random.seed(0)
+data = np.random.binomial(50, 0.6, 100).tolist()
+b1 = Binomial(data)
+print('P(30):', b1.pmf(30))
+
+b2 = Binomial(n=50, p=0.6)
+print('P(30):', b2.pmf(30))
