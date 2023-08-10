72ac7f3d0487ef48febbf1387445299e72d1b842
rioquentin <quentin.rio.pro@gmail.com>
2023-08-10 22:58:14 +0000

    Task 12 - Create cdf function

--- math/probability/main.py
+++ math/probability/main.py
@@ -6,7 +6,7 @@ Binomial = __import__('binomial').Binomial
 np.random.seed(0)
 data = np.random.binomial(50, 0.6, 100).tolist()
 b1 = Binomial(data)
-print('P(30):', b1.pmf(30))
+print('F(30):', b1.cdf(30))
 
 b2 = Binomial(n=50, p=0.6)
-print('P(30):', b2.pmf(30))
+print('F(30):', b2.cdf(30))
