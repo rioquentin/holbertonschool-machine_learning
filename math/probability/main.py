409621d977e70957232655a089c148629d2222a9
rioquentin <quentin.rio.pro@gmail.com>
2023-08-10 13:44:35 +0000

    Task 10 - Create a class Binomial

--- math/probability/main.py
+++ math/probability/main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-
 import numpy as np
 Binomial = __import__('binomial').Binomial
 
-np.random.seed(0)
-data = np.random.binomial(50, 0.6, 100).tolist()
-b1 = Binomial(data)
-print('n:', b1.n, "p:", b1.p)
-
-b2 = Binomial(n=50, p=0.6)
-print('n:', b2.n, "p:", b2.p)
+np.random.seed(6)
+p = np.random.uniform(0.01, 0.99)
+n = np.random.randint(1, 100)
+s = np.random.randint(100, 1000)
+data = np.random.binomial(n, p, s).tolist()
+b = Binomial()
+print(b.n, b.p)
+b = Binomial(data)
+print(b.n, np.around(b.p, 10))
+b = Binomial(n=n, p=p)
+print(b.n, np.around(b.p, 10))
