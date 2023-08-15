cc086c30e212e2934ae3c74b8ce75241f2381ccf
rioquentin <quentin.rio.pro@gmail.com>
2023-08-15 19:02:36 +0000

    Task 0

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -19,6 +19,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.nx = nx
-        self.W = np.random.normal(size=self.nx)
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
