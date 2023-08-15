87464032f3220a1d606d2d9795b89637fb7e4fed
rioquentin <quentin.rio.pro@gmail.com>
2023-08-15 18:07:34 +0000

    Task 0

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Task 0 - Write a class Neuron
+"""
+
+import numpy as np
+
+
+class Neuron:
+
+    def __init__(self, nx):
+        """
+        Class constructor
+        """
+
+        if not isinstance(nx, int) and not isinstance(nx, float):
+            raise TypeError("nx must be an integer or a float")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.W = np.random.normal(size=self.nx)
+        self.b = 0
+        self.A = 0
