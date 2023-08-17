64ab004aec495f18753ff9cde7e54c697a2e16af
rioquentin <quentin.rio.pro@gmail.com>
2023-08-17 13:18:39 +0000

    Task 6 - Train the model

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -93,3 +93,22 @@ class Neuron:
         self.__b -= alpha * db
 
         return self.__W, self.__b
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """
+        Function to train the model
+        """
+
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for _ in range(iterations):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+
+        return self.evaluate(X, Y)
