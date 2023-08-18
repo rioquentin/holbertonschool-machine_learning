5ee27c7dcae3b251e2038703c74e447c74b5af7f
rioquentin <quentin.rio.pro@gmail.com>
2023-08-18 12:49:39 +0000

    Task 9 - Make NN variables private

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+"""
+Task 8 Define Neural Network class
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    NeuralNetwork Class
+    """
+
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+    @property
+    def W1(self):
+        """
+        Getter for W
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+        Getter for b
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+        Getter for A
+        """
+        return self.__A1
+        
+    @property
+    def W2(self):
+        """
+        Getter for W
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+        Getter for b
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+        Getter for A
+        """
+        return self.__A2
