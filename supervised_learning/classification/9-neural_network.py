2583453ca17ea7afa9b094c1e7b933c99ce93ea9
rioquentin <quentin.rio.pro@gmail.com>
2023-08-18 12:50:56 +0000

    Task 9 - Fixed styles

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -28,44 +28,45 @@ class NeuralNetwork:
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
+
     @property
     def W1(self):
         """
-        Getter for W
+        Getter for W1
         """
         return self.__W1
 
     @property
     def b1(self):
         """
-        Getter for b
+        Getter for b1
         """
         return self.__b1
 
     @property
     def A1(self):
         """
-        Getter for A
+        Getter for A1
         """
         return self.__A1
-        
+
     @property
     def W2(self):
         """
-        Getter for W
+        Getter for W2
         """
         return self.__W2
 
     @property
     def b2(self):
         """
-        Getter for b
+        Getter for b2
         """
         return self.__b2
 
     @property
     def A2(self):
         """
-        Getter for A
+        Getter for A2
         """
         return self.__A2
