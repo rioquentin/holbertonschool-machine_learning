1e18a78ad5408b7f93d9930fe8816dcdc6160ed4
rioquentin <quentin.rio.pro@gmail.com>
2023-08-19 18:26:19 +0000

    Task 16 - Test

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -6,11 +6,12 @@ Task 16 - DeepNeuralNetwork
 
 import numpy as np
 
+
 class DeepNeuralNetwork:
     """
     DeepNeuralNetwork Class
     """
-    
+
     def __init__(self, nx, layers):
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -18,14 +19,12 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or layers is None:
             raise TypeError("layers must be a list of positive integers")
-        
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        
-        for i in range(1, self.L):
-            # Initialize weights using He et al. initialization
-            self.weights["W{}".format(i)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2.0 / layers[i - 1])
-            
-            # Initialize biases to zeros
-            self.weights["b{}".format(i)] = np.zeros((layers[i], 1))
+
+        for i in range(0, self.L):
+            self.weights["W{}".format(
+                i + 1)] = np.random.randn(layers[i], layers[i - 1])
+            self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
