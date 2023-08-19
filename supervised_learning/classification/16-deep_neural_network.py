ffc844d97795263a4d25c52a12f03d4badd60b1b
rioquentin <quentin.rio.pro@gmail.com>
2023-08-19 18:46:09 +0000

    Task 16 - Test

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -17,7 +17,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx <= 0:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or layers is None:
+        if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
@@ -25,6 +25,8 @@ class DeepNeuralNetwork:
         self.weights = {}
 
         for l in range(1, self.L + 1):
+            if not isinstance(layers[l - 1], int) or layers[l - 1] <= 0:
+                raise TypeError("layers must be a list of positive integers")
             self.weights['W' +
                          str(l)] = np.random.randn(layers[l - 1], nx) * np.sqrt(2/nx)
             self.weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
--- supervised_learning/classification/main.py
+++ supervised_learning/classification/main.py
@@ -4,15 +4,12 @@ import numpy as np
 Deep = __import__('16-deep_neural_network').DeepNeuralNetwork
 
 np.random.seed(16)
-nx = np.random.randint(100, 1000)
-l = np.random.randint(3, 10)
-sizes = np.random.randint(5, 20, l - 1).tolist()
-sizes.append(1)
-d = Deep(nx, sizes)
-print('cache:')
-for k, v in sorted(d.cache.items()):
-    print(k, v)
-print('weights:')
-for k, v in sorted(d.weights.items()):
-    print(k, v)
-print(d.L)
+nx = np.random.randint(3, 10)
+try:
+    d = Deep(nx, [3, 5, -1])
+    print('FAIL')
+except TypeError as e:
+    if str(e) == 'layers must be a list of positive integers':
+        print('OK', end='')
+    else:
+        print('FAIL')
