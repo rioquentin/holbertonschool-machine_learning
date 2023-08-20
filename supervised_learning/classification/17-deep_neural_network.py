23a5f5ce77e91eaca7275a187303c562c62a059b
rioquentin <quentin.rio.pro@gmail.com>
2023-08-20 15:54:30 +0000

    Task 17 - Set attribute to private

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""
+Task 16 - DeepNeuralNetwork
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    DeepNeuralNetwork Class
+    """
+
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or not layers:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for l in range(1, self.L + 1):
+            if not isinstance(layers[l - 1], int) or layers[l - 1] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            W = np.random.randn(layers[l - 1], nx) * np.sqrt(2/nx)
+            b = np.zeros((layers[l - 1], 1))
+            self.weights['W' + str(l)] = W
+            self.weights['b' + str(l)] = b
+            nx = layers[l - 1]
+
+    @property
+    def L(self):
+        """
+        Getter for L
+        """
+        return self.__L
+
+    @property
+    def cache(self):
+        """
+        Getter for cache
+        """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """
+        Getter for weights
+        """
+        return self.__weights
