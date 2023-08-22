bc230e03b349a9662866e28953a27eb8ce95940f
rioquentin <quentin.rio.pro@gmail.com>
2023-08-22 12:35:39 +0000

    Task 25 - Decode One hot

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+"""
+Task 25 -  One-Hot Decode
+"""
+
+
+def one_hot_decode(one_hot):
+    """
+    Decode 
+    """
+
+    if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
+        return None
+
+    return np.argmax(one_hot, axis=0)
