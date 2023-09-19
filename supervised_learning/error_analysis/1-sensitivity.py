11d8d109ae51222714e37fc08ed0b77775fb1250
rioquentin <quentin.rio.pro@gmail.com>
2023-09-19 12:29:33 +0000

    Task 1 - pycode

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -7,6 +7,7 @@ Returns:
 """
 import numpy as np
 
+
 def sensitivity(confusion):
     """ calculates the sensitivity for each class in a confusion matrix """
     return np.diagonal(confusion) / np.sum(confusion, axis=1)
