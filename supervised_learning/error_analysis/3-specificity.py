Task 3 - wrong axis

supervised_learning/error_analysis/3-specificity.py
@@ -10,4 +10,4 @@ import numpy as np
 
 def specificity(confusion):
     """ calculates the specificity for each class in a confusion matrix """
-    return np.diagonal(confusion) / np.sum(confusion, axis=0)
+    return np.diagonal(confusion) / np.sum(confusion, axis=1)