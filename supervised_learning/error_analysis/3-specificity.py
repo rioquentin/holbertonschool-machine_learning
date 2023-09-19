beaacaf11d7fe4303c42d35706410ab3a263acbf
rioquentin <quentin.rio.pro@gmail.com>
2023-09-19 12:37:54 +0000

    Task 3 - specificity

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""_summary_
+
+    Returns:
+        _type_: _description_
+    """
+import numpy as np
+
+
+def specificity(confusion):
+    """ calculates the specificity for each class in a confusion matrix """
+    return np.diagonal(confusion) / np.sum(confusion, axis=0)
